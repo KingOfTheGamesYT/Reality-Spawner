8ef4f7cd380be7f89ecba6f58dd0d034a30da2f0
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2025-10-15 11:55:49 -0400

    initial set up
    for 1.16.5

---
+++ README.md
@@ -0,0 +1,9 @@
+# Random Dungeon Spawners
+This is a mod that adds a machine that allows you to take pieces of other realities and bring them into this world
+
+The license is ARR
+
+- You can not claim this mod as your own
+- Please report any bugs you experience by using this
+- You may make videos on it
+- You may use it in modpacks
---
+++ src/main/java/com/devmaster/reality_spawner/misc/Reality_Spawner.java
@@ -0,0 +1,40 @@
+package com.devmaster.reality_spawner.misc;
+
+import net.minecraft.entity.EntityType;
+
+import net.minecraftforge.common.DungeonHooks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mod("reality_spawner")
+public class Reality_Spawner {
+    public static final Logger LOGGER = LogManager.getLogger("Reality Spawner");
+    public static final String MOD_ID = "reality_spawner";
+
+    public Reality_Spawner() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+}
